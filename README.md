4fd6e9ed022b289197a0956e1f5168829f3555d5
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-05-30 15:06:06 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pronamic-woocommerce-subscriptions-period
-This "Woo Subscriptions" add-on ensures that a period is saved with each subscription order. The "Woo Subscriptions" plugin itself does not store the subscription period in an order. Without this add-on, it is not clear to webshop owners and customers for what period the subscription order and associated payment was.
+# Pronamic period information for Woo Subscriptions
+
+This “Woo Subscriptions” add-on ensures that a period is saved with each subscription order. The “Woo Subscriptions” plugin itself does not store the subscription period in an order. Without this add-on, it is not clear to webshop owners and customers for what period the subscription order and associated payment was.
+
+## Links
+
+- https://github.com/pronamic/woocommerce-subscriptions/issues/10
---
+++ pronamic-woocommerce-subscriptions-period.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Pronamic period information for Woo Subscriptions
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-woocommerce-subscriptions-period/
+ * Description: This “Woo Subscriptions” add-on ensures that a period is saved with each subscription order.
+ *
+ * Version: 1.0.0
+ * Requires at least: 5.9
+ * Requires PHP: 8.1
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-woocommerce-subscriptions-period
+ * Domain Path: /languages/
+ *
+ * License: GPL-2.0-or-later
+ *
+ * GitHub URI: https://github.com/pronamic/pronamic-woocommerce-subscriptions-period
+ *
+ * WC requires at least: 8.0
+ * WC tested up to: 8.0
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WooSubscriptionsPeriod
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
