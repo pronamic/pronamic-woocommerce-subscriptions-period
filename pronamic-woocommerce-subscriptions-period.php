482d529f606805f5aed1a22e2698e71e6b09d651
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-05-30 16:50:02 +0200

    Update pronamic-woocommerce-subscriptions-period.php

--- pronamic-woocommerce-subscriptions-period.php
+++ pronamic-woocommerce-subscriptions-period.php
@@ -27,6 +27,12 @@
  * @package   Pronamic\WooSubscriptionsPeriod
  */
 
+namespace Pronamic\WooSubscriptionsPeriod;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use WC_Order;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -34,7 +40,133 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Bootstrap.
  */
+final class Plugin {
+	/**
+	 * Setup.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		\add_action( 'init', [ $this, 'init' ] );
+	}
 
+	/**
+	 * Initialize.
+	 * 
+	 * @return void
+	 */
+	public function init() {
+		if ( \is_admin() ) {
+			\add_action( 'add_meta_boxes', [ $this, 'maybe_add_pronamic_meta_box_to_wc_order' ], 10, 2 );
+		}
+
+		\add_filter( 'wcs_new_order_created', [ $this, 'wcs_new_order_created' ], 10, 2 );
+
+		\add_filter( 'wcs_renewal_order_created2', function( $id, $test ) {
+			echo 'hoi';exit;
+		}, 10, 2 );
+
+		\add_action( 'woocommerce_checkout_update_order_meta', [ $this, 'woocommerce_checkout_update_order_meta'] );
+	}
+
+
+	/**
+	 * Maybe add a Pronamic meta box the WooCommerce order.
+	 * 
+	 * @link https://github.com/pronamic/wp-pronamic-pay-woocommerce/issues/41
+	 * @link https://developer.wordpress.org/reference/hooks/add_meta_boxes/
+	 * @param string           $post_type_or_screen_id Post type or screen ID.
+	 * @param WC_Order|WP_Post $post_or_order_object   Post or order object.
+	 * @return void
+	 */
+	public function maybe_add_pronamic_meta_box_to_wc_order( $post_type_or_screen_id, $post_or_order_object ) {
+		if ( ! \in_array( $post_type_or_screen_id, [ 'shop_order', 'woocommerce_page_wc-orders' ], true ) ) {
+			return;
+		}
+
+		$order = $post_or_order_object instanceof WC_Order ? $post_or_order_object : \wc_get_order( $post_or_order_object->ID );
+
+		if ( ! $order instanceof WC_Order ) {
+			return;
+		}
+
+		\add_meta_box(
+			'woocommerce-order-pronamic-period',
+			\__( 'Period', 'pronamic-woocommerce-subscriptions-period' ),
+			function () use ( $order ) {
+				$start_date = $order->get_meta( '_pronamic_period_start_date' );
+				$end_date   = $order->get_meta( '_pronamic_period_end_date' );
+
+				var_dump( $start_date );
+				var_dump( $end_date );
+			},
+			$post_type_or_screen_id,
+			'side',
+			'default'
+		);
+	}
+
+	/**
+	 * Woo Subscriptions renewal order created.
+	 * 
+	 * @link https://github.com/Automattic/woocommerce-subscriptions-core/blob/c2bf2c565a3b77b8cf1c0cb254a56f9039fa0ab6/includes/wcs-order-functions.php#L232-L239
+	 * @link https://github.com/Automattic/woocommerce-subscriptions-core/blob/c2bf2c565a3b77b8cf1c0cb254a56f9039fa0ab6/includes/wcs-renewal-functions.php#L38C25-L38C50
+	 * @param WC_Order        $new_order    The new order created from the subscription.
+	 * @param WC_Subscription $subscription The subscription the order was created from.
+	 * @return WC_Order
+	 */
+	public function wcs_new_order_created( $new_order, $subscription ) {
+		$this->update_period_meta( $new_order, $subscription );
+
+		return $new_order;
+	}
+
+	private function update_period_meta( $order, $subscription ) {
+		$next_payment_date_timestamp_1 = $subscription->get_time( 'next_payment' );
+
+		if ( 0 === $next_payment_date_timestamp_1 ) {
+			return;
+		} 
+
+		$next_payment_date_timestamp_2 = \wcs_add_time(
+			$subscription->get_billing_interval(),
+			$subscription->get_billing_period(),
+			$next_payment_date_timestamp_1
+		);
+
+		$start_date = new DateTimeImmutable( '@' . $next_payment_date_timestamp_1, new DateTimeZone( 'UTC' ) );
+		$end_date   = new DateTimeImmutable( '@' . $next_payment_date_timestamp_2, new DateTimeZone( 'UTC' ) );
+
+		$order->update_meta_data( '_pronamic_period_start_date', $start_date->format( 'Y-m-d H:i:s' ) );
+		$order->update_meta_data( '_pronamic_period_end_date', $end_date->format( 'Y-m-d H:i:s' ) );
+
+		$order->save();
+
+	}
+
+	/**
+	 * Order.
+	 *
+	 * @link https://github.com/woocommerce/woocommerce/blob/1d593e3d8933ba3834be06d2371ae34d14a08e0c/plugins/woocommerce/includes/class-wc-checkout.php#L461-L466
+	 */
+	public function woocommerce_checkout_update_order_meta( $order_id ) {
+		$order = \wc_get_order( $order_id );
+
+		if ( ! $order instanceof WC_Order ) {
+			return;
+		}
+
+		$subscriptions = \wcs_get_subscriptions_for_order( $order );
+
+		foreach ( $subscriptions as $subscription ) {
+			$this->update_period_meta( $order, $subscription );
+		}
+	}
+}
+
+$pronamic_wcsp = new Plugin();
+
+$pronamic_wcsp->setup();
 
 /**
  * High Performance Order Storage.
