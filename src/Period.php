d7016e4ad801d01caac6d3ead04e9e100f45d67a
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-06-05 14:59:31 +0200

    Add `from_woocommerce_order_item( WC_Order_Item $item )` helper function.

--- src/Period.php
+++ src/Period.php
@@ -11,6 +11,8 @@
 namespace Pronamic\WooSubscriptionsPeriod;
 
 use DateTimeImmutable;
+use DateTimeZone;
+use WC_Order_Item;
 
 /**
  * Plugin class
@@ -39,5 +41,42 @@ final class Period {
 	public function __construct( DateTimeImmutable $start_date, DateTimeImmutable $end_date ) {
 		$this->start_date = $start_date;
 		$this->end_date   = $end_date;
+	}
+
+	/**
+	 * Retrieve period from WooCommerce order item.
+	 * 
+	 * @param WC_Order_Item $item WooCommerce order item.
+	 * @return Period|null
+	 */
+	public static function from_woocommerce_order_item( WC_Order_Item $item ) {
+		$start_date_string = $item->get_meta( '_pronamic_start_date' );
+
+		if ( ! \is_string( $start_date_string ) ) {
+			return null;
+		}
+
+		if ( '' === $start_date_string ) {
+			return null;
+		}
+
+		$end_date_string = $item->get_meta( '_pronamic_end_date' );
+
+		if ( ! \is_string( $end_date_string ) ) {
+			return null;
+		}
+
+		if ( '' === $end_date_string ) {
+			return null;
+		}
+
+		try {
+			$start_date = new DateTimeImmutable( $start_date_string, new DateTimeZone( 'UTC' ) );
+			$end_date   = new DateTimeImmutable( $end_date_string, new DateTimeZone( 'UTC' ) );
+
+			return new self( $start_date, $end_date );
+		} catch ( \Exception $e ) {
+			return null;
+		}
 	}
 }
